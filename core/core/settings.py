826cdb64235f7e058de3b95e2839b2dc6997d683
Jayson Pardilla <codewithdocsonji16@gmail.com>
2026-09-18 00:20:11 +0800

    change domain name

--- core/core/settings.py
+++ core/core/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-d!328^d((@e2_61s13s3q9=1w!
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.up.railway.app,3kub0-production.up.railway.app').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'ekubo-marketplace.up.railway.app').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
